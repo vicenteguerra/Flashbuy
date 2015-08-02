f09e76ace4d8a9f0657ea83cf39ad6314a1b2077
Javier Telio <jtelio118@gmail.com>
2015-08-01 21:31:26 -0500

    add seeds

--- Jerry/database/seeds/DatabaseSeeder.php
+++ Jerry/database/seeds/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         // $this->call(UserTableSeeder::class);
+        $this->call(CreditCardTypeTableSeeder::class);
+        $this->call(CustomerTypeTableSeeder::class);
 
         Model::reguard();
     }
