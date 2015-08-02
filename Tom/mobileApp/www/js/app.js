d3d6905f1c05ab3e9a737531e7a071f9e4d33937
vicenteguerra <vicenteguerrahdez@gmail.com>
2015-08-02 11:36:40 -0500

    scanner code

--- Tom/mobileApp/www/js/app.js
+++ Tom/mobileApp/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic','ngCordova', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -124,7 +124,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     views: {
       'tab-payments': {
         templateUrl: 'templates/charge.html',
-        controller: 'chargeCtrl'
+        controller: 'scannerController'
       }
     }
   })
--- Tom/mobileApp/www/js/controllers.js
+++ Tom/mobileApp/www/js/controllers.js
@@ -96,6 +96,20 @@ angular.module('starter.controllers', [])
 
 })
 
+.controller("scannerController", function($scope, $cordovaBarcodeScanner) {
+
+    $scope.scanBarcode = function() {
+        $cordovaBarcodeScanner.scan().then(function(imageData) {
+            alert(imageData.text);
+            console.log("Barcode Format -> " + imageData.format);
+            console.log("Cancelled -> " + imageData.cancelled);
+        }, function(error) {
+            console.log("An error happened -> " + error);
+        });
+    };
+
+})
+
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
