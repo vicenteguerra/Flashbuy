8948d6b878ecc2f0d79237b2b30c8e5a51354c50
Javier Telio <jtelio118@gmail.com>
2015-08-01 21:30:33 -0500

    add models

---
+++ Jerry/app/CustomerType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerType extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'customers_type';
+
+    public $timestamps = false;
+
+}
