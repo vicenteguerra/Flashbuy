164f12d310b675f0ecac9adc85147782e8e306d9
vicenteguerra <vicenteguerrahdez@gmail.com>
2015-08-01 19:08:51 -0500

    API controllers

--- Jerry/app/Http/routes.php
+++ Jerry/app/Http/routes.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('customer', 'Api\customerController');
+Route::resource('payment', 'Api\paymentController');
+Route::resource('social', 'Api\socialController');
